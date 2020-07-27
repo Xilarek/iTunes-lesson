bdba105da6bdf23a0f1ec115815db292700da400
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-07-27 12:48:10 +0300

    itunes lesson one

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const playerBtn = document.querySelectorAll('.player-btn'),
+          playerBlock = document.querySelectorAll('.player-block'),
+          temp = document.querySelector('.temp');
+
+        const deactivationPlayer = () => {
+            temp.style.display = 'none';
+            playerBlock.forEach( item => item.classList.remove('active'));
+            playerBtn.forEach( item => item.classList.remove('active'));
+        };
+    
+        playerBtn.forEach((btn, i) => {
+            
+            btn.addEventListener('click', () => {
+                deactivationPlayer();
+                btn.classList.add('active');
+                playerBlock[i].classList.add('active');
+            });
+        });
+});
+
+
+
